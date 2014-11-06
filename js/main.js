37d5371f5535afa1cbb2c9df255bb35c0e0742d7
finbarleahy <finbarleahy@gmail.com>
2014-11-06 18:51:18 +0000

    Adding a little movement by bouncing a ball onto the grid.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
-var red = "#dc463f";
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function CircleGenerator(ballCount, ballsPerLine, radius)
 {
@@ -18,10 +31,12 @@ function CircleGenerator(ballCount, ballsPerLine, radius)
 
             dataset.push(
                 {
+                    "id": "circle" + getRandomInt(0, 4),
                     "cx": (columnIndex * this.cxSpacing),
                     "cy": cyPosition,
                     "radius": this.radius,
-                    "color" : "steelblue" }
+                    "color" : ((i < 1980) ? "steelblue" : "#dc463f")
+                }
             );
         }
         return dataset;
@@ -33,6 +48,7 @@ var svgContainer = d3.select("body").select("svg");
 var generator =  new CircleGenerator(3225, 115, 2.5);
 var source = svgContainer
     .append("g")
+    .attr("id", "source")
     .attr("transform", "translate(80,0)")
     .selectAll("circle")
     .data(generator.getCircles())
@@ -41,16 +57,48 @@ var source = svgContainer
     .attr("cx", function (d) { return d.cx; })
     .attr("cy", function (d) { return d.cy; })
     .attr("r", function (d) { return d.radius; })
+    .attr("id", function (d, i) { return i;})
     .style("fill", function (d) { return d.color; });
 
-generator =  new CircleGenerator(1250, 115, 2.5, 5);
+var destinationGenerator = new CircleGenerator(2, 2, 2.5, 5);
 var destination = svgContainer.append("g")
     .attr("transform", "translate(80,270)")
     .selectAll("circle")
-    .data(genDestination.getCircles())
+    .data(destinationGenerator.getCircles())
     .enter()
     .append("circle")
     .attr("cx", function (d) { return d.cx; })
     .attr("cy", function (d) { return d.cy; })
     .attr("r", function (d) { return d.radius; })
+    .attr("id", function (d, i) { return "c" + i;})
     .style("fill", function (d) { return d.color; });
+
+var cx = d3.selectAll("g").selectAll("#c1").attr("cx");
+var cy = d3.selectAll("g").selectAll("#c1").attr("cy");
+var tempCx = 100;
+var tempCy = -100;
+
+$('.alternate').click(function ()
+{
+    var temp;
+    temp = cx, cx = tempCx, tempCx = temp;
+    temp = cy, cy = tempCy, tempCy = temp;
+
+    try {
+        d3.selectAll("g")
+            .selectAll("#c1")
+            .transition()
+            .attr("cx", cx)
+            .attr("cy", cy)
+            .ease("elastic")
+            .duration(4000)
+            .delay(0);
+    }
+    catch (err) {
+        $("#errorMessage").text(err);
+    }
+});
+
+function swap(a, b){
+
+}
